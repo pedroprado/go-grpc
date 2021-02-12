d7dbe4d81081fef2a8139824a2f03fd194670d30
pedroprado_bexs <pedro.prado@bexsbanco.com.br>
2021-02-12 18:13:18 -0300

    Add simple grpc client

--- client/main.go
+++ client/main.go
@@ -1,5 +1,34 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"google.golang.org/grpc"
+	pb "pedro.prado.grpc.client.example/infra/grpc/protoFile"
+)
+
 func main() {
 
+	var opts []grpc.DialOption
+	opts = append(opts, grpc.WithInsecure())
+
+	conn, err := grpc.Dial(fmt.Sprintf("localhost:%d", 8950), opts...)
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+
+	defer conn.Close()
+
+	client := pb.NewRouteGuideClient(conn)
+	ctx := context.Background()
+
+	fmt.Println("Rodando GetFeature")
+	feature, err := client.GetFeature(ctx, &pb.Point{Latitue: 2, Longitue: 3})
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+
+	fmt.Printf("%v\n", feature)
 }
